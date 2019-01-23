cdcec24556502450a5cc9db74b628f72d7b9e363
raja <rajaratnadeep@hotmail.com>
2019-01-23 21:23:41 +0530

    Add POST /users route and token method and delete todo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rajam:hey123@ds111065.mlab.com:11065/raja01', {useNewUrlParser: true}) //|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+}) //'mongodb://rajam:hey123@ds111065.mlab.com:11065/raja01' ||
     .then(()=> console.log('MongoDb Connected'))
     .catch(err => console.log(err));
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+var _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -50,6 +51,40 @@ app.get('/todos/:id', (req,res)=>{
         res.status(400).send();
     })
 })
+
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+})
+
+//POST /users
+app.post('/users', (req,res)=>{
+    var body = _.pick(req.body, ['email','password']);
+    var user = new User(body);
+
+
+
+    user.save().then(()=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth', token).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+});
 
 app.listen(port,()=>{
     console.log(`Started up at port ${port}`);
