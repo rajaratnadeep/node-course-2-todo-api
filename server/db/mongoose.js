8c4878d908e30b7caae74987f87f62c8bb5bdea5
raja <rajaratnadeep@hotmail.com>
2019-01-23 12:16:32 +0530

    Ready for heroku 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://rajamahapatra01@gmail.com:bangalore123@ds111065.mlab.com:11065/raja01');//|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
