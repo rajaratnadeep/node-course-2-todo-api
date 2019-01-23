dd9a2550ea28921ff30f8e2baafc7fcd1634338e
raja <rajaratnadeep@hotmail.com>
2019-01-23 13:31:04 +0530

    heroku 5 authentication prob solved

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rajam:bangalore123@ds111065.mlab.com:11065/raja01', {useNewUrlParser: true});//|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://rajam:hey123@ds111065.mlab.com:11065/raja01', {useNewUrlParser: true}) //|| 'mongodb://localhost:27017/TodoApp');
+    .then(()=> console.log('MongoDb Connected'))
+    .catch(err => console.log(err));
 
 module.exports = {mongoose};
