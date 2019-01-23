e392242cc68743b78df9b8a61f7227f8e898395f
raja <rajaratnadeep@hotmail.com>
2019-01-23 12:31:11 +0530

    Heroku 4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rajam:bangalore123@ds111065.mlab.com:11065/raja01', {useNewUrlParser: true});//|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://rajam:bangalore123@ds111065.mlab.com:11065/raja01', {useNewUrlParser: true} );//|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
